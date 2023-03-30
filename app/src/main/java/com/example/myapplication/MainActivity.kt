e8373d1bd7dcb179d8a3ea87c905d18489ea5a3c
PC670 <akzumrad@gmail.com>
2023-03-30 13:20:35 +0500

    д

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction().add(R.id.container, BlankFragment()).commit()
+       binding.homeFragment.setOnClickListener {
+           supportFragmentManager.beginTransaction().replace(R.id.container, BlankFragment()).commit()
+       }
+        binding.profileFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.container, ProfileFragment()).commit()
+        }
+
     }
 }
